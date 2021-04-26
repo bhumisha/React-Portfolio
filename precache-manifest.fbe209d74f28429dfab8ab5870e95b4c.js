b8c655a72451c4a385a8fa69fb87c78315a7a401
Bhumisha Dave <bhumishadave@yahoo.com>
2021-04-25 23:25:52 -0700

    Updates

---
+++ precache-manifest.fbe209d74f28429dfab8ab5870e95b4c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba2f453c48706af58503c12dba4bf26",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "6cbb073a9dce0301ef32",
+    "url": "/React-Portfolio/static/css/main.23731556.chunk.css"
+  },
+  {
+    "revision": "c3ed377b11ed4a6fc6ab",
+    "url": "/React-Portfolio/static/js/2.8f3803d3.chunk.js"
+  },
+  {
+    "revision": "64f54ce285484345cd5e3bd96b120148",
+    "url": "/React-Portfolio/static/js/2.8f3803d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cbb073a9dce0301ef32",
+    "url": "/React-Portfolio/static/js/main.7a56b331.chunk.js"
+  },
+  {
+    "revision": "53cd5964469ae1da263b",
+    "url": "/React-Portfolio/static/js/runtime-main.01560ae1.js"
+  },
+  {
+    "revision": "fbdf1a3a22acf8c566ebf36d58ec9b9a",
+    "url": "/React-Portfolio/static/media/BhumishaDave.fbdf1a3a.jpg"
+  },
+  {
+    "revision": "609037618c993cb7e401e1339d4cd198",
+    "url": "/React-Portfolio/static/media/PortfolioPic1.60903761.png"
+  },
+  {
+    "revision": "83ab26488f3abd5983950c73b2d7c2b9",
+    "url": "/React-Portfolio/static/media/ThreesaCrowd.83ab2648.png"
+  },
+  {
+    "revision": "de19b6609c91a5e42d493864102584aa",
+    "url": "/React-Portfolio/static/media/calc.de19b660.png"
+  },
+  {
+    "revision": "501111f72e284f8e670e1787aa389d4a",
+    "url": "/React-Portfolio/static/media/calculator1.501111f7.png"
+  },
+  {
+    "revision": "ffdef9c11c27bb0843c0b832685ad38c",
+    "url": "/React-Portfolio/static/media/horiseon.ffdef9c1.png"
+  },
+  {
+    "revision": "81b39f5287a80383b39c754cbbce9368",
+    "url": "/React-Portfolio/static/media/simpleSuds.81b39f52.png"
+  },
+  {
+    "revision": "f5e3a0baa35160c3b852691fb936971e",
+    "url": "/React-Portfolio/static/media/weatherReport.f5e3a0ba.png"
+  }
+]);
